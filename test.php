37c9cf90c026fedebfc707b2af35c3591b5cffd0
haehyun <haehyuncho@gmail.com>
2020-11-06 13:37:59 -0700

    test.php

--- test.php
+++ test.php
@@ -1,5 +1,1 @@
-   $output = array();
-   $command = "cat /etc/passwd | cut -d\":\" -f1";
-   echo 'running the command: <b>'.$command."</b><br />";
-   exec($command, &$output);
-   echo implode("<br />\n", $output);
+echo "hello";
